16b7d21072b79ed8e853d5f07d21912914caf31f
Alen-Berg <alexei.hlatsko@gmail.com>
2024-05-04 20:21:27 +0200

    qr fix

--- src/pages/Scan.jsx
+++ src/pages/Scan.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import {QrReader} from 'react-qr-reader';
 
 const Scan = () => {
   const navigate = useNavigate();
+  const [result, setResult] = useState(null);
+  const [facingMode, setFacingMode] = useState('environment'); // Начальное значение: обычная камера
+  const qrRef = useRef(null); // Создаем ссылку на компонент QrReader
 
-  const handleScan = (data) => {
+  const handleScan = async (data) => {
     if (data) {
-      // Парсим JSON из полученных данных
-      const productData = JSON.parse(data);
-
-      // Формируем путь с параметрами для редиректа
-      const path = `/product-details?id=${productData.fields.id.stringValue}&title=${productData.fields.title.stringValue}&desc=${productData.fields.desc.stringValue}&price=${productData.fields.price.integerValue}`;
-
-      // Редирект на страницу ProductDetails с параметрами пути
-      navigate(path);
+      try {
+        // Выполняем GET-запрос к URL продукта
+        const response = await axios.get(data);
+        console.log('Product data:', response.data);
+        const { fields } = response.data;
+        
+        // Создаем строку параметров запроса на основе данных из fields
+        const queryParams = Object.entries(fields)
+          .map(([key, value]) => `${key}=${encodeURIComponent(value.stringValue || value.integerValue)}`)
+          .join('&');
+        
+        // Редирект на страницу ProductDetails с параметрами запроса
+        navigate(`/product-details?${queryParams}`);
+      } catch (error) {
+        console.error('Error fetching product data:', error);
+      }
     }
   };
 
   const handleError = (err) => {
     console.error(err);
+  };
+
+  const toggleFacingMode = () => {
+    // Переключение камеры между фронтальной и обычной
+    setFacingMode((prevMode) => (prevMode === 'environment' ? 'user' : 'environment'));
   };
 
   return (
@@ -28,10 +45,12 @@ const Scan = () => {
       <QrReader
         delay={300}
         onError={handleError}
-        onScan={handleScan}
-        videoConstraints={videoConstraints}
+        onResult={handleScan}
+        facingMode={facingMode} // Устанавливаем режим камеры
         style={{ width: '100%' }}
       />
+       {/* Кнопка для переключения камеры */}
+       <button onClick={toggleFacingMode}>Toggle Camera</button>
     </div>
   );
 };
